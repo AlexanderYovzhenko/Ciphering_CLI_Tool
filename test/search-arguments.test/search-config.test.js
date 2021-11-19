8508422002750e0d4c32a28527e636fea17fdf17
Alexander <renyreny125tt@gmail.com>
2021-11-19 13:29:38 +0300

    test: add search arguments testing

---
+++ test/search-arguments.test/search-config.test.js
@@ -0,0 +1,30 @@
+const searchConfig = require('../../src/search-arguments/search-config');
+
+function forEach(items, callback) {
+  for (let index = 0; index < items.length; index++) {
+    callback(items[index]);
+  }
+}
+
+const mockSearchConfig = jest.fn(searchConfig);
+
+describe('Search config', () => {
+  let arrArgumentsOne = [['-c','C1-R0', '-i', '-o'], ['--config', '--input', '--output'], ['-i','./input.txt', '--config', 'C1-A', '--output', './output.txt']];
+  let arrArgumentsTwo = [['-i', '-o'], ['--input', '--output'], ['-i', './input.txt', '--output', './output.txt']];
+
+  forEach(arrArgumentsOne, mockSearchConfig);
+  forEach(arrArgumentsTwo, mockSearchConfig);
+
+  test('should value is not false', () => {
+    expect(mockSearchConfig.mock.results[0].value).not.toBe(false);
+  });
+
+  test('should be equal to', () => {
+    expect(mockSearchConfig.mock.results[0].value).toBe('C1-R0');
+    expect(mockSearchConfig.mock.results[2].value).toBe('C1-A');
+  });  
+
+  test('should value is false', () => {
+    expect(mockSearchConfig.mock.results[5].value).toBe(false);
+  });
+});
